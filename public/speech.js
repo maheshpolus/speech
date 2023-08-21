08b379488f2909d5e3c85716c0f9410f8ece35e1
Mahesh Sreenath <34370397+maheshpolus@users.noreply.github.com>
2023-08-21 21:18:25 +0530

    test

--- public/speech.js
+++ public/speech.js
@@ -1,18 +1,13 @@
-/* eslint-disable eqeqeq */
-/* eslint-disable no-multi-str */
-/* eslint-disable no-unused-vars */
-/* eslint-disable default-case */
-var SpeechSDK;
-var authorizationToken = 'eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJjZW50cmFsaW5kaWEiLCJzdWJzY3JpcHRpb24taWQiOiJmMDg3MWNjMTQ3MzQ0MTJhODYwYmEwNDhkMWVmNjVhYSIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5GMCIsImNvZ25pdGl2ZS1zZXJ2aWNlcy1lbmRwb2ludCI6Imh0dHBzOi8vYXBpLmNvZ25pdGl2ZS5taWNyb3NvZnQuY29tL2ludGVybmFsL3YxLjAvIiwiYXp1cmUtcmVzb3VyY2UtaWQiOiIvc3Vic2NyaXB0aW9ucy9hMmMzZmM4Ny1hZTUyLTRhMDUtODU4Yy02NGMwZTI1NWNkZmIvcmVzb3VyY2VHcm91cHMvc3BlZWNoL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29nbml0aXZlU2VydmljZXMvYWNjb3VudHMvc3BlZWNoLXRvLXRleHQtZWR1Iiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy5jZW50cmFsaW5kaWEiLCJleHAiOjE2OTI2MjUxMzksImlzcyI6InVybjptcy5jb2duaXRpdmVzZXJ2aWNlcyJ9.U6J-W_tG8Q7MuS_fwOxNqfnIaO9GiEfXEWorSOuL54QdKofsHFhR19fDhWm1v79mjtku0aG9WbJSkKJTpgpNcQ';
-var regionOptions = 'centralindia';
-var languageOptions = 'es-ES'; 
-var scenarioSelection = 'pronunciationAssessmentOnce'; 
-var reco;
-var referenceText = 'Hola, me llamo Jorge';
-var authorizationEndpoint = 'Speech/GetAuthorizationKey';
-var saveSpeechEndPoint = 'Speech/SaveSpeech';
-var lanugaeEndPoint = 'Speech/GetLanguage'
-var soundContext = undefined;
+let SpeechSDK;
+let authorizationToken = 'eyJhbGciOiJFUzI1NiIsImtpZCI6ImtleTEiLCJ0eXAiOiJKV1QifQ.eyJyZWdpb24iOiJjZW50cmFsaW5kaWEiLCJzdWJzY3JpcHRpb24taWQiOiJmMDg3MWNjMTQ3MzQ0MTJhODYwYmEwNDhkMWVmNjVhYSIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5GMCIsImNvZ25pdGl2ZS1zZXJ2aWNlcy1lbmRwb2ludCI6Imh0dHBzOi8vYXBpLmNvZ25pdGl2ZS5taWNyb3NvZnQuY29tL2ludGVybmFsL3YxLjAvIiwiYXp1cmUtcmVzb3VyY2UtaWQiOiIvc3Vic2NyaXB0aW9ucy9hMmMzZmM4Ny1hZTUyLTRhMDUtODU4Yy02NGMwZTI1NWNkZmIvcmVzb3VyY2VHcm91cHMvc3BlZWNoL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29nbml0aXZlU2VydmljZXMvYWNjb3VudHMvc3BlZWNoLXRvLXRleHQtZWR1Iiwic2NvcGUiOiJzcGVlY2hzZXJ2aWNlcyIsImF1ZCI6InVybjptcy5zcGVlY2hzZXJ2aWNlcy5jZW50cmFsaW5kaWEiLCJleHAiOjE2OTI2MzMzNjQsImlzcyI6InVybjptcy5jb2duaXRpdmVzZXJ2aWNlcyJ9.MpH9s6F5VyCpwAYb5ny9RhVQfp5Ry0N15EMzoUY9wCuPdR6m1-XfE1m8InNe88Lahx7TMnjnxjLMhGG4hUrjfw';
+let regionOptions = 'centralindia';
+let languageOptions = 'es-ES'; 
+let reco;
+let referenceText = 'Hola, me llamo Jorge';
+let authorizationEndpoint = 'Speech/GetAuthorizationKey';
+let saveSpeechEndPoint = 'Speech/SaveSpeech';
+let lanugaeEndPoint = 'Speech/GetLanguage'
+let soundContext;
 
 async function RequestAuthorizationToken() {
     try {
@@ -27,9 +22,8 @@ async function RequestAuthorizationToken() {
 
 async function RequestLanguage() {
     try {
-        const res = await fetch (authorizationEndpoint);
+        const res = await fetch (lanugaeEndPoint);
         const token = await res.json();
-        authorizationToken = token;
         console.log('Token fetched from back-end: ' + token);
     } catch (err) {
         console.log(err);
@@ -37,12 +31,12 @@ async function RequestLanguage() {
 }
 
 function Initialize(onComplete) {
-    if (!!window.SpeechSDK) {
+    if (window.SpeechSDK) {
         onComplete(window.SpeechSDK);
     }
 }
 try {
-    var AudioContext = window.AudioContext // our preferred impl
+    let AudioContext = window.AudioContext // our preferred impl
         || window.webkitAudioContext       // fallback, mostly when on Safari
         || false;                          // could not find.
     if (AudioContext) {
@@ -77,20 +71,9 @@ function getSpeechConfig(sdkConfigType) {
 }
 
 function getPronunciationAssessmentConfig() {
-    var pronunciationAssessmentConfig = new SpeechSDK.PronunciationAssessmentConfig(referenceText,
+    let pronunciationAssessmentConfig = new SpeechSDK.PronunciationAssessmentConfig(referenceText,
         SpeechSDK.PronunciationAssessmentGradingSystem.HundredMark,
         SpeechSDK.PronunciationAssessmentGranularity.Word, true);
-    return pronunciationAssessmentConfig;
-}
-
-function getPronunciationAssessmentConfigFromJson() {
-    var pronunciationAssessmentConfig = new SpeechSDK.PronunciationAssessmentConfig.fromJSON(
-        "{\"GradingSystem\": \"HundredMark\", \
-                \"Granularity\": \"Phoneme\", \
-                \"EnableMiscue\": \"True\", \
-                \"ScenarioId\": \"[scenario ID will be assigned by product team]\"}"
-    );
-    pronunciationAssessmentConfig.referenceText = referenceText;
     return pronunciationAssessmentConfig;
 }
 
@@ -99,8 +82,8 @@ function onRecognizing(sender, recognitionEventArgs) {
 }
 
 function onRecognized(sender, recognitionEventArgs) {
-    var result = recognitionEventArgs.result;
-    onRecognizedResult(recognitionEventArgs.result);
+    let result = recognitionEventArgs.result;
+    onRecognizedResult(result);
 }
 
 function onRecognizedResult(result) {
@@ -128,11 +111,10 @@ function applyCommonConfigurationTo(recognizer) {
     recognizer.sessionStopped = onSessionStopped;
 }
 
-
 function doPronunciationAssessmentOnceAsync() {
-    var audioConfig = getAudioConfig();
-    var speechConfig = getSpeechConfig(SpeechSDK.SpeechConfig);
-    var pronunciationAssessmentConfig = getPronunciationAssessmentConfig();
+    let audioConfig = getAudioConfig();
+    let speechConfig = getSpeechConfig(SpeechSDK.SpeechConfig);
+    let pronunciationAssessmentConfig = getPronunciationAssessmentConfig();
     if (!audioConfig || !speechConfig || !pronunciationAssessmentConfig) return;
     reco = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
     applyCommonConfigurationTo(reco);
@@ -150,9 +132,12 @@ function doPronunciationAssessmentOnceAsync() {
 async function saveSpeechToServer(data) {
     if (saveSpeechEndPoint) {
         try {
-            const res = await fetch(saveSpeechEndPoint, data);
-            const movies = await res.json();
-            console.log('speech Saved sucessfullyy');
+            const res = await fetch(saveSpeechEndPoint, {
+                method: 'POST',
+                body: JSON.stringify(data) 
+            });
+            const response = await res.json();
+            console.log(response);
         } catch (err) {
             console.log(err);
         }
